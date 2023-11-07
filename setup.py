6104e58a4a2c6dabb70b7976480db2e5584b8af3
whoami <igorkzolov1992@gmail.com>
2023-11-07 13:46:52 +0500

    1.0.63

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ class CMakeBuild(build_ext):
             build_temp.mkdir(parents=True)
 
         vcpkg_path = os.path.join(ext.source_dir, 'vcpkg')
+        print(f'vcpkg path is {vcpkg_path}')
         if not os.path.exists(vcpkg_path) or os.listdir(vcpkg_path) == 0:
             print(f'{vcpkg_path} path does not exist or empty')
 
